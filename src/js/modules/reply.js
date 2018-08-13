78b048333865f0a73a9cfca4193f86fb9fd7f64a
skiprox <seanmichaelscanlan@gmail.com>
2018-08-13 17:47:07 -0400

    Starting work on reply

--- src/js/modules/reply.js
+++ src/js/modules/reply.js
@@ -7,14 +7,21 @@ class Reply {
     constructor() {
         this.emailReply = document.getElementById('email-reply');
         this.sendBtn = document.getElementById('reply-send');
+        this.closeBtn = document.getElementById('reply-close-btn');
         this.emailReply.style.display = 'block';
         this.onSendBtnClick = this.onSendBtnClick.bind(this);
+        this.onCloseBtnClick = this.onCloseBtnClick.bind(this);
         this.addListeners();
     }
     addListeners() {
         this.sendBtn.addEventListener('click', this.onSendBtnClick);
+        this.closeBtn.addEventListener('click', this.onCloseBtnClick);
     }
     onSendBtnClick(e) {
+        e.preventDefault();
+        this.destroy();
+    }
+    onCloseBtnClick(e) {
         e.preventDefault();
         this.destroy();
     }
